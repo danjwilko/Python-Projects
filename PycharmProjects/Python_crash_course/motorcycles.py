8c5d340e7e0dfda0d206243dbbb7033fa76b2243
Daniel Wilkinson <danielwilkinson0991@gmail.com>
2024-05-27 08:05:03 +0100

    updates

--- PycharmProjects/Python_crash_course/motorcycles.py
+++ PycharmProjects/Python_crash_course/motorcycles.py
@@ -52,6 +52,17 @@ popped_motorcycles = motorcycles.pop()
 print(motorcycles)
 print(popped_motorcycles)
 
+motorcycles = ['honda', 'yamaha', 'suzuki']
+
+#last_owned = motorcycles.pop()
+#print(f"The last motorcycle I owned was a {last_owned.title()}")
+
+first_owned = motorcycles.pop(0)
+print(f"The first motorcycle I owned was a {first_owned.title()}")
+
+
+
+
 
 
 
