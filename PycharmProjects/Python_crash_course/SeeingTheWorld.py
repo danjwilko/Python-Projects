26480c58d1f74e461a22cb4265e5cb6b8f5fdc0f
Daniel Wilkinson <danielwilkinson0991@gmail.com>
2024-05-28 19:23:04 +0100

    python crash course work

---
+++ PycharmProjects/Python_crash_course/SeeingTheWorld.py
@@ -0,0 +1,17 @@
+places_to_visit = ['Venice', 'New York', 'Krakow', 'London', 'Japan']
+
+print(places_to_visit)
+print(sorted(places_to_visit))
+print(places_to_visit)
+places_to_visit.reverse()
+print(places_to_visit)
+
+places_to_visit.reverse()
+print(places_to_visit)
+
+places_to_visit.sort()
+print(places_to_visit)
+places_to_visit.sort(reverse=True)
+print(places_to_visit)
+
+print(len(places_to_visit))
