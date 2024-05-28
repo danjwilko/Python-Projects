398dad3beaa1eff08831637b7e38aff024492022
Daniel Wilkinson <danielwilkinson0991@gmail.com>
2024-05-28 14:00:39 +0100

    python crash course work

--- PycharmProjects/Python_crash_course/cars.py
+++ PycharmProjects/Python_crash_course/cars.py
@@ -4,6 +4,13 @@ cars = ['bmw', 'audi', 'toyota', 'subaru']
 # cars.sort()
 # print(cars)
 
-cars.sort(reverse=True)
-print(cars)
+# the sort() method permenantly alters the list.
+# cars.sort(reverse=True)
+# print(cars)
 
+# To sort a list temporarily we can us the sorted() function
+
+# print("Here is the original list")
+# print(cars)
+# print(sorted(cars))
+# print(cars)
