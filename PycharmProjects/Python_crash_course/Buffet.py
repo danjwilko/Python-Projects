92e97f9f7d1b9b04edcb9cd55d0d34c9478368e1
Daniel Wilkinson <danielwilkinson0991@gmail.com>
2024-05-31 19:27:18 +0100

    python crash course work

---
+++ PycharmProjects/Python_crash_course/Buffet.py
@@ -0,0 +1,10 @@
+menu = ('pizza', 'pasta', 'fish and chips','pie', 'jacket potato')
+
+for food in menu:
+    print(food)
+
+# menu[0] = 'sandwich'
+
+menu = ('sandwich', 'cheesecake', 'pasta', 'fish and chips','pie', 'jacket potato')
+for food in menu:
+    print(food)
--- PycharmProjects/Python_crash_course/main.py
+++ PycharmProjects/Python_crash_course/main.py
@@ -1,34 +1,16 @@
-# Defining a tuple
-""" A tuple looks just like a list except we use parenthesis instead of square brackets."""
+# Styling your code.
 
-dimensions = (200, 50)
-print(dimensions[0])
-print(dimensions[1])
+# The style guide- pep8
 
-# when trying to modify a tuple we get an error as it cannot be done with that type.
-# dimensions[0] = 250
+# Indentation
+""" Pep 8 reccomends that you use four spaces for each level of indentation"""
 
-# note Tuples are technically defined by the presence of a comma; the parentheses make them
-# look neater and more readable. If you want to define a tuple with 1 element you need to include a trailing comma.
+# Line Length
 
-# my_t = (3,)
+"""The recommendation that each line of a program, should be no longer than 80 characters in length.
+The recommendation for comment length is comments should be no longer than 72 characters in length.
+"""
 
-# Looping through all the values in a tuple.
+# Blank lines
 
-#for dimension in dimensions:
-#    print(dimension)
-
-# Writing over a tuple.
-
-# Although you cannot modify a tuple, you can assign a new variable that represents a tuple.
-# So if we wanted to change our dimensions, we could redefine the entire tuple.
-# In this way were not modifying the existing tuple but redefining the variable.
-
-print('Original dimensions:')
-for dimension in dimensions:
-    print(dimension)
-
-dimensions = (400, 100)
-
-for dimension in dimensions:
-    print(dimension)
+"""To group parts of a program use blank lines, dont use excessive lines"""
