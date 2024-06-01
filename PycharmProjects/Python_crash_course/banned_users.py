7ee5d465c258e484963e2c7e5d468373dbd102c4
Daniel Wilkinson <danielwilkinson0991@gmail.com>
2024-06-01 06:43:44 +0100

    python crash course work

---
+++ PycharmProjects/Python_crash_course/banned_users.py
@@ -0,0 +1,12 @@
+banned_users = ['andrew', 'carolina', 'david']
+
+user = 'marie'
+
+if user not in banned_users:
+    print(f"{user.title()}, you can post a response if you wish.")
+
+
+# Boolean expressions
+
+# A boolean expression is just another name for a conditional test, a boolean value is either True or False.
+
