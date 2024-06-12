388fc8e46768b16dfe1b07e3e236e46af4230f82
Daniel Wilkinson <danielwilkinson0991@gmail.com>
2024-06-12 21:58:29 +0100

    course updates

--- PycharmProjects/100daysofcoding/ceaser_cipher.py
+++ PycharmProjects/100daysofcoding/ceaser_cipher.py
@@ -3,22 +3,55 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
             'q', 'r', 's', 't', 'u',
             'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""
 
 
-#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
 def caeser(start_text, shift_amount, cipher_direction):
     end_text = ''
     if cipher_direction == 'decode':
         shift_amount *= -1
-    for letter in start_text:
-        position = alphabet.index(letter)
-        new_position = position + shift_amount
-        end_text = end_text + alphabet[new_position]
+    for char in start_text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = position + shift_amount
+            end_text = end_text + alphabet[new_position]
+        else:
+            end_text = end_text + char
     print(f"The {cipher_direction}d text is {end_text}")
 
+
+print(logo)
+
+end_cipher = False
+while not end_cipher:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    shift = shift % 26
+
+    caeser(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+    response = input("Type 'yes' if you want to go again. Otherwise type 'no'.")
+    if response == "no":
+        end_cipher = True
+        print("Goodbye")
 
 """ My verson was this:
 def caeser(text, shift):
@@ -32,7 +65,6 @@ def caeser(text, shift):
                 index = (position - shift)
             encode_letter = alphabet[index]
             encode_string = encode_string + encode_letter
-    print(encode_string)"""
+    print(encode_string)
 
-#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message.
-caeser(start_text=text, shift_amount=shift, cipher_direction=direction)
+caeser(text, shift)"""
