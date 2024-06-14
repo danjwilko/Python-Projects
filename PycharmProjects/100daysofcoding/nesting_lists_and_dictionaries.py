5eb7cf5a589c36f671f50c5abe76a325bdfe51c9
Daniel Wilkinson <danielwilkinson0991@gmail.com>
2024-06-14 12:32:04 +0100

    course updates

--- PycharmProjects/100daysofcoding/nesting_lists_and_dictionaries.py
+++ PycharmProjects/100daysofcoding/nesting_lists_and_dictionaries.py
@@ -15,3 +15,16 @@ travel_log = {
     "France": {"cities_visited": ["Paris", "Lille", "Dijon"], "total_visits": 12},
     "Germany": {"cities_visited": ["Berlin", "Hamburg", "Stuttgart"], "total_visits": 6},
 }
+
+# Nesting a dictionary in a list
+travel_log = [
+    {
+        "country": "france",
+        "cities_visited": ["Paris", "Lille", "Dijon"],
+        "total_visits": 12
+    },
+    {
+        "country": "Germany",
+        "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+        "total_visits": 6},
+]
