6f0be8506f7f4fb4ea7026f193e83afeccca52f1
Daniel Wilkinson <danielwilkinson0991@gmail.com>
2024-06-19 09:23:44 +0100

    file changes to revert

--- PycharmProjects/Git_practice/Testing_branch.py
+++ PycharmProjects/Git_practice/Testing_branch.py
@@ -4,6 +4,10 @@ this will also include a file which we will modify and revert back to previous c
 
 print("Adding a file for testing purposes on the testing branch")
 
-# succesful branch created.
+# Successful branch created.
 
 print("branch and files created for branch")
+
+# We will now add a line of code which we will commit and then revert.
+
+print("This line of code will be part of the commit which we will revert back to.")
