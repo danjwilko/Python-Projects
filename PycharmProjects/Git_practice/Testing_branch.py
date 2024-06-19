e31a94c50bef9caa3a856389cbbe518d88a4d0ec
Daniel Wilkinson <danielwilkinson0991@gmail.com>
2024-06-19 09:52:08 +0100

    Revert "Revert "file changes to revert""
    This reverts commit daa97fff6d9c05cdf2e3203f840b52eb54877eb0.

--- PycharmProjects/Git_practice/Testing_branch.py
+++ PycharmProjects/Git_practice/Testing_branch.py
@@ -2,7 +2,6 @@
 """ Created a branch for testing purposes which we will merge at a later date,
 this will also include a file which we will modify and revert back to previous commit"""
 
-<<<<<<< Testing_branch
 print("Adding a file for testing purposes on the testing branch")
 
 # Successful branch created.
@@ -12,6 +11,3 @@ print("branch and files created for branch")
 # We will now add a line of code which we will commit and then revert.
 
 print("This line of code will be part of the commit which we will revert back to.")
-=======
-print("Adding a file for testing purposes on the testing branch")
->>>>>>> 2682cc5 additional file modifications for testing branch
