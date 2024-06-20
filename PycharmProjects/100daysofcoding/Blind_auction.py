720542ca42d6a2b15867424bf4c97b0461533ed6
Daniel Wilkinson <danielwilkinson0991@gmail.com>
2024-06-20 15:26:03 +0100

    Creating test files

--- PycharmProjects/100daysofcoding/Blind_auction.py
+++ PycharmProjects/100daysofcoding/Blind_auction.py
@@ -17,15 +17,27 @@ logo = '''
 
 print(logo)
 bids = {}
-
 bidding_finished = False
-while not bidding_finished:
-    bidder_name = input("What is your name? ")
-    bid_price = int(input("What is your bid? "))
 
+
+def find_highest_bidder(bidding_record):
+    highest_bid = 0
+    winner = ""
+    for bidder in bidding_record:
+        bid_amount = bidding_record[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidder.capitalize()
+    print(f"The winner is {winner} with a bid of ${highest_bid}")
+
+
+while not bidding_finished:
+    bidder_name = input("What is your name?: ")
+    bid_price = int(input("What is your bid?: $"))
+    bids[bidder_name] = bid_price
     should_continue = input("Are there any more bidders? type 'yes' or 'no'.")
     if should_continue.lower() == "no":
         bidding_finished = True
+        find_highest_bidder(bids)
     elif should_continue == "yes":
         clear()
-
