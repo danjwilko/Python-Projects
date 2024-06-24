9ffb7ce7e05a0c811ce0c5aec9546e15b1b16d6a
Daniel Wilkinson <danielwilkinson0991@gmail.com>
2024-06-24 13:25:19 +0100

    Course updates

--- PycharmProjects/100daysofcoding/Calculator.py
+++ PycharmProjects/100daysofcoding/Calculator.py
@@ -15,9 +15,6 @@ logo = """
 |_____________________|
 """
 
-print(logo)
-
-
 # Add
 def add(n1, n2):
     return n1 + n2
@@ -45,11 +42,27 @@ operations = {
     "/": divide
 }
 
-num1 = int(input("What's your first number?"))
+def calculator():
+    print(logo)
 
-for symbol in operations:
-    print(symbol)
-operation_symbol = input("Pick an operation from the line above")
-num2 = int(input("What's your second number?"))
-calculation_function = operations[operation_symbol]
-calculation_function(num1, num2)
+    num1 = float(input("What's your first number?: "))
+
+    for symbol in operations:
+        print(symbol)
+    should_continue = True
+
+    while should_continue:
+        operation_symbol = input("Pick an operation: ")
+        num2 = float(input("What's the next number?: "))
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(num1, num2)
+
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+        if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation.: ") == "y":
+            num1 = answer
+        else:
+            should_continue = False
+            calculator()
+
+calculator()
