42a0ee131a279ecf9fb48ecc1f54153743355530
daniel <danielwilkinson0991@gmail.com>
2024-07-15 13:48:32 +0100

    Global constants naming conventions and start of guessing the number game.

---
+++ PycharmProjects/100daysofcoding/Global_constants.py
@@ -0,0 +1,7 @@
+# Global constants
+
+# Global constants naming conventions are all uppercase.
+
+PI = 3.141592653589793
+URL = "https://en.wikipedia.org/wiki/Global_constants"
+TWITTER_HANDLE = "yhg_reneger"
---
+++ PycharmProjects/100daysofcoding/Number_guessing_game.py
@@ -0,0 +1,10 @@
+# Number guessing game objectives
+
+# Include ASCII art logo.
+# Allow the player to submit a guess for a number between 1 and 100
+# Check user's guess against actual answer. Print "Too high." or "To low". depending on the user's answer.
+# If they got the answer correct, show the actual answer to the player.
+# Track the number of turns remaining.
+# If they run out of turns, provide feedback to the player.
+# Include two different difficulty levels (e.g., 10 guesses in easy mode, only 5 in the hard mode).
+
