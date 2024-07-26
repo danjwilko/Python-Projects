a7fd545a28a1be856fdee26b0a5f0a34a4df9974
daniel <danielwilkinson0991@gmail.com>
2024-07-26 07:17:04 +0100

    course updates

--- PycharmProjects/Python_crash_course/glossary.py
+++ PycharmProjects/Python_crash_course/glossary.py
@@ -1,6 +1,19 @@
 # Glossary
 
 glossary = {'dictionary': 'Items are stored as key-value pairs',
-            'key-value pair': 'items in a dictionary have a key, which we can look up the value associated with.'
+            'key-value pair': 'items in a dictionary have a key, which we can look up the value associated with.',
+            'conditional tests': 'At the heart of every if statement is an expression that can be evaluated as True '
+                                 'of False and is called a conditional test.',
+
+            'boolean expression': 'boolean expression is just another name for a conditional test, a boolean value is '
+                                  'either "True" or "False".',
+            'range()': 'returns a sequence of numbers, starting from 0 by default, and increments by 1 (by default), '
+                       'and stops before a specified number.',
 
             }
+
+print(f"A dictionary is: {glossary['dictionary']}")
+print(f"A key-value pair is: {glossary['key-value pair']}")
+print(f"A conditional test is: {glossary['conditional tests']}")
+print(f"A boolean expression is: {glossary['boolean expression']}")
+print(f"A range() function is: {glossary['range()']}")
--- PycharmProjects/Python_crash_course/person.py
+++
@@ -1,3 +0,0 @@
-person = {'first_name': 'Bob', 'last_name': 'Smith', 'age': 25, 'city': 'New York'}
-
-print(person['first_name'], person['last_name'], person['age'], person['city'])
