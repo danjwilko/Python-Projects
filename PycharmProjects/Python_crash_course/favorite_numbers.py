c5695ef93969564dab60254380c9147d737b0dd2
daniel <danielwilkinson0991@gmail.com>
2024-07-26 06:58:10 +0100

    course updates

---
+++ PycharmProjects/Python_crash_course/favorite_numbers.py
@@ -0,0 +1,17 @@
+# Peoples favorite numbers stored in a dictionary
+
+favorite_numbers = {'Bob': 5,
+                    'Alice': 4,
+                    'Charlie': 10,
+                    'Emma': 20,
+                    'Steve': 8
+                    }
+
+print(f"Bob's favorite number is {favorite_numbers['Bob']}")
+print(f"Alice's favorite number is {favorite_numbers['Alice']}")
+print(f"Steve's favorite number is {favorite_numbers['Charlie']}")
+print(f"Bob's favorite number is {favorite_numbers['Emma']}")
+print(f"Alice's favorite number is {favorite_numbers['Steve']}")
+
+
+
