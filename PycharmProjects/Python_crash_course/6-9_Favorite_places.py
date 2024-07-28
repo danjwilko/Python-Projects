74b1ad308b60214c3f05e2848cfa9fc920c6daef
daniel <danielwilkinson0991@gmail.com>
2024-07-28 19:13:17 +0100

    Chapter 6 lessons and start of exercies

---
+++ PycharmProjects/Python_crash_course/6-9_Favorite_places.py
@@ -0,0 +1,12 @@
+# Dictionary containing a list of places.
+favorite_places = {
+    'bob': ['Spain', 'Italy'],
+    'lisa': ['Belguim'],
+    'phil':['France', 'Switzerland']
+}
+
+for name, places in favorite_places.items():
+    print(f"{name.title()}'s favorite places are: ")
+    for place in places:
+        print(f"\t{place.title()}")
+
