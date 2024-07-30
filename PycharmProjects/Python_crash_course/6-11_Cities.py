a094e304dfe1b495c9cb204c4f8c3fbbf4db5cfe
daniel <danielwilkinson0991@gmail.com>
2024-07-30 18:02:51 +0100

    Chapter 6 lessons continuation of exercies

--- PycharmProjects/Python_crash_course/6-11_Cities.py
+++ PycharmProjects/Python_crash_course/6-11_Cities.py
@@ -1,3 +1,9 @@
 cities = {
-
+    'paris': {'country': 'france', 'population': '1.161 million'},
+    'berlin': {'country': 'germany', 'population': '3.645 million'},
+    'madrid': {'country': 'spain', 'population': '3.223 million'}
 }
+
+for city, details in cities.items():
+    print(f"The city of {city.title()} is in {details['country'].title()} and"
+          f" has a population of {details['population']} people.")
