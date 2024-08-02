e98e523b427a2cc6af7915a78475b4daa7e77cfe
daniel <danielwilkinson0991@gmail.com>
2024-08-02 07:49:21 +0100

    chapter 7 lessons and exercises.

---
+++ PycharmProjects/Python_crash_course/counting.py
@@ -0,0 +1,10 @@
+# Introducing while loops.
+
+# We can use a while loop to count up through a series of numbers.
+current_number = 1
+# Python will repeat the loop as long as the condition is met.
+while current_number <= 5:
+    print(current_number)
+    current_number += 1
+
+# This program will print out each number from 1-5 inclusive on a new line.
