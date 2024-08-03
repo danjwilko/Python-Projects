904ef502bb18ca2c42ce4bb516702ba71fa8d47b
daniel <danielwilkinson0991@gmail.com>
2024-08-03 13:50:48 +0100

    chapter 7 lessons and exercises.

--- PycharmProjects/Python_crash_course/7-4_Pizza_Toppings.py
+++ PycharmProjects/Python_crash_course/7-4_Pizza_Toppings.py
@@ -1,8 +1,9 @@
-prompt = input("Please enter a pizza topping you would like to add: ")
-prompt += "\nEnter 'quit' when you have finished "
+prompt = "Please enter a pizza topping you would like to add: "
+prompt += "\nEnter 'quit' when you have finished. "
 
-message = ""
-while message != "quit":
-    message = input(prompt)
-    if message != "quit":
-        print(message)
+while True:
+    topping = input(prompt)
+    if topping != "quit":
+        print(f"I'll add {topping} to your pizza toppings.")
+    else:
+        break
