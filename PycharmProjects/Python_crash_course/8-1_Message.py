99d7eeb51f3718e5b5eff69797645ab8df1aa0df
daniel <danielwilkinson0991@gmail.com>
2024-08-05 07:36:16 +0100

    Chapter 8 Exercises.

---
+++ PycharmProjects/Python_crash_course/8-1_Message.py
@@ -0,0 +1,8 @@
+# Chapter 8 exercises.
+
+def display_message():
+    """Display a message about what im studying."""
+    print("Hello everyone! In this chapter, I'm learning about python functions.")
+
+
+display_message()
---
+++ PycharmProjects/Python_crash_course/8-2_Favorite_Book.py
@@ -0,0 +1,8 @@
+# favorite book message.
+
+def favorite_book(title_of_favorite_book):
+    """Prints a message outlining favorite book"""
+    print(f"One of my favorite books is {title_of_favorite_book.title()}")
+
+
+favorite_book("The lord of the Rings")
