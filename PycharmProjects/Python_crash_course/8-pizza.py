050a0dfc363f21f1dffd3336ec13ee40c34ffea3
daniel <danielwilkinson0991@gmail.com>
2024-08-07 11:21:49 +0100

    Chapter 8 Exercises.

--- PycharmProjects/Python_crash_course/8-pizza.py
+++ PycharmProjects/Python_crash_course/8-pizza.py
@@ -1,4 +1,4 @@
-# Passing an arbitary number of arguments.
+# Passing an arbitrary number of arguments.
 
 # def make_pizza(*toppings):
 #     """Print the list of toppings that have been requested."""
@@ -16,5 +16,27 @@ def make_pizza(*toppings):
     for topping in toppings:
         print(f"- {topping}")
 
+
 make_pizza('pepperoni')
 make_pizza('mushrooms', 'green pepper', 'extra cheese')
+
+
+# Mixing positional and arbitrary arguments.
+
+# If we want a function to accept several different kinds of arguments, the parameter that accepts an arbitrary number
+# of arguments must be placed last in the function definition.
+# Python matches positional and keyword arguments first, then collects any remaining arguments in the final parameter.
+
+
+def make_pizza(size, *toppings):
+    """Summarise the pizza we are about to make"""
+    print(f"\nMaking a {size}-inch pizza with the following toppings: ")
+    for topping in toppings:
+        print(f"- {topping}")
+
+
+make_pizza(16, 'pepperoni')
+make_pizza(12, 'mushrooms', 'green pepper', 'extra cheese')
+
+# Note: You will often see the generic parameter name *args, which collects arbitrary positional arguments like this.
+
