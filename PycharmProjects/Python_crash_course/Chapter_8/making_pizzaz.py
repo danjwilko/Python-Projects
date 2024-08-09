2b4eb3904f2a55ec76d74e1b918783eccdd110a7
daniel <danielwilkinson0991@gmail.com>
2024-08-09 07:52:13 +0100

    Storing and importing modules in python chapter 8 exercises.

--- PycharmProjects/Python_crash_course/Chapter_8/making_pizzaz.py
+++ PycharmProjects/Python_crash_course/Chapter_8/making_pizzaz.py
@@ -3,3 +3,55 @@ import pizza
 
 pizza.make_pizza(16, 'pepperoni')
 pizza.make_pizza(12, 'mushrooms', 'green pepper', 'extra cheese')
+
+"""Importing specific functions."""
+# You can also import specific function from a module. The general syntax:
+# from module_name import function_name
+
+# You can also import aa many functions as you want from a module by separating each function with a comma.
+# from module_name import function_0, function_1, function_2
+
+# This is what the pizza.py file would look like if we just imported the function we're going to use.
+from pizza import make_pizza
+
+make_pizza(16, 'pepperoni')
+make_pizza(12, 'pepperoni', 'mushrooms', 'green pepper', 'extra cheese')
+
+
+"""Using as to give a function an Alias"""
+
+# The name of your function were importing may conflict with a existing name being used in the program or if the
+# function name is realy long.
+
+from pizza import make_pizza as mp
+
+mp(16, 'pepperoni')
+mp(12, 'pepperoni', 'mushrooms', 'green pepper', 'extra cheese')
+
+# The general syntax for providing and alias is:
+# from module_name import function_name as fn
+
+"""Using as to give a Module an alias"""
+
+# We can also provide an alias for a module name. Using a shorter alias can make the code more concise.
+
+import pizza as p
+
+# Using the alias we have p.make_pizza() rather than pizza.make_pizza().
+p.make_pizza(16, 'pepperoni')
+p.make_pizza(12, 'pepperoni', 'mushrooms', 'green pepper', 'extra cheese')
+
+# The general syntax for this approach is:
+# import module_name as mn
+
+"""Importing all the functions in a module"""
+# We can tell Python to import every function in a module by using the asterisk (*) operator.
+
+from pizza import *
+
+make_pizza(16, 'pepperoni')
+make_pizza(12, 'pepperoni', 'mushrooms', 'green pepper',)
+
+# Note its always best to try an umport only the functions you want, or import the entrire module and use dot notation.
+# General syntax is:
+# from module_name import.
