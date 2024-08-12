0ee27242091cf3faab889d790f25b9ae270ece8b
daniel <danielwilkinson0991@gmail.com>
2024-08-12 07:54:25 +0100

    chapter 9 classes lessons

--- PycharmProjects/Python_crash_course/Chapter_9/car.py
+++ PycharmProjects/Python_crash_course/Chapter_9/car.py
@@ -80,11 +80,26 @@ class Car:
         print(f"This car has {self.odometer_reading} miles on it.")
 
     def update_odometer(self, mileage):
-        """Set the odometer reading to the given value"""
-        self.odometer_reading = mileage
+        """Set the odometer reading to the given value
+        Reject the change if it attempts to roll the odometer back"""
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll the odometer back!")
 
 
 my_new_car = Car('Audi', 'a4', 2024)
 print(my_new_car.get_descriptive_name())
 my_new_car.update_odometer(23)
 my_new_car.read_odometer()
+
+my_used_car = Car('subaru', 'outback', 2015)
+print(my_used_car.get_descriptive_name())
+
+my_used_car.update_odometer(23_500)
+my_used_car.read_odometer()
+
+my_used_car.update_odometer(100)
+my_used_car.read_odometer()
+
+
