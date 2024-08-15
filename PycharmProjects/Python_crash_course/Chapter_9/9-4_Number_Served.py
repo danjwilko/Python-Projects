da2419cd76706e0437ed55896613eeac6682556e
daniel <danielwilkinson0991@gmail.com>
2024-08-15 07:45:00 +0100

    chapter 9 classes lessons

---
+++ PycharmProjects/Python_crash_course/Chapter_9/9-4_Number_Served.py
@@ -0,0 +1,28 @@
+class Restaurant():
+    """A simple Restaurant model"""
+
+    def __init__(self, restaurant_name, cuisine_type):
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+        self.number_served = 0
+
+    def describe_restaurant(self):
+        print(f"{self.restaurant_name}, serves {self.cuisine_type} food.")
+
+    def open_restaurant(self):
+        print(f"{self.restaurant_name}, is open!")
+
+    def get_number_served(self):
+        print(f"The restaurant has served {self.number_served} customers.")
+
+    def set_number_served(self, number):
+        self.number_served += number
+
+
+restaurant = Restaurant("Tarantino's", "italian")
+restaurant.describe_restaurant()
+restaurant.open_restaurant()
+restaurant.set_number_served(5)
+restaurant.get_number_served()
+
+
