a439db638589216889298bc466e6ecd63ac0518b
daniel <danielwilkinson0991@gmail.com>
2024-08-21 21:43:09 +0100

    Chapter 9 Exercises

--- PycharmProjects/Python_crash_course/Chapter_9/9-12_Multiple_Modules/setup_admin.py
+++ PycharmProjects/Python_crash_course/Chapter_9/9-12_Multiple_Modules/setup_admin.py
@@ -1,2 +1,15 @@
 from admin import Admin
-import user
+
+john = Admin('John', 'Smith', '21', 'user21js', 'Smith', 'Alaska')
+john.describe_user()
+
+john.privileges.show_privileges()
+
+print("\nAdding privileges...")
+john_privileges = ['can add post',
+                   'can delete post',
+                   'can ban user'
+                   ]
+john.privileges.privileges = john_privileges
+print(f" The admin {john.username} has these privileges: ")
+john.privileges.show_privileges()
